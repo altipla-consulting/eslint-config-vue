1ac78383f066d32a9d522978c6cf284dd184e99d
Ernesto Alejo <ernestokarim@gmail.com>
2021-05-14 02:22:18 +0200

    Update to Vue 3 recommendations.

--- index.js
+++ index.js
@@ -7,26 +7,26 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'plugin:vue/essential'
+    'plugin:vue/essential',
   ],
   parserOptions: {
     ecmaVersion: 11,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: ['vue'],
   ignorePatterns: ['*.css'],
   rules: {
     'indent': ['error', 2],
     'linebreak-style': ['error', 'unix'],
-    'quotes': ['error', 'single', {allowTemplateLiterals: true}],
+    'quotes': ['error', 'single', { allowTemplateLiterals: true }],
     'semi': ['error', 'never'],
     'comma-dangle': ['error', 'always-multiline'],
     'dot-notation': 'error',
     'eol-last': 'error',
     'key-spacing': 'error',
-    'keyword-spacing': ['error', {'before': true, 'after': true}],
+    'keyword-spacing': ['error', { before: true, after: true }],
     'no-undef': 'error',
-    'no-unused-vars': ['error', {'varsIgnorePattern': '_', 'argsIgnorePattern': '_'}],
+    'no-unused-vars': ['error', { varsIgnorePattern: '_', argsIgnorePattern: '_' }],
     'space-before-blocks': 'error',
     'space-infix-ops': 'error',
     'object-curly-spacing': ['error', 'always'],
@@ -34,5 +34,7 @@ module.exports = {
     // Disable root validation until it adapts to Vue 3 where we can have multiple
     // of them. I guess this rule will disappear once that occurs.
     'vue/valid-template-root': ['off'],
-  }
-};
+    'vue/no-multiple-template-root': ['off'],
+    'vue/no-v-for-template-key': ['off'],
+  },
+}
